fed74419225b9d5e97eb26157dbdb5950a8a6b83
masadi <masadi.com@gmail.com>
2024-06-30 04:54:55 +0700

    Update

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -37,9 +37,9 @@ class ChatBotController extends Controller
     }
     public function sendWhatsAppMessage(string $message, string $recipient)
     {
-        $twilio_whatsapp_number = getenv('TWILIO_WHATSAPP_NUMBER');
-        $account_sid = getenv("TWILIO_SID");
-        $auth_token = getenv("TWILIO_AUTH_TOKEN");
+        $twilio_whatsapp_number = env('TWILIO_WHATSAPP_NUMBER');
+        $account_sid = env("TWILIO_SID");
+        $auth_token = env("TWILIO_AUTH_TOKEN");
 
         $client = new Client($account_sid, $auth_token);
         return $client->messages->create($recipient, array('from' => "whatsapp+:$twilio_whatsapp_number", 'body' => $message));
